862f2c630d92b1fa8946ba48146f216aaa7e0c85
Alumno <Alumno@DESKTOP-IIC5HVT>
2016-09-29 07:25:27 -0500

    CajeroMain

---
+++ Cajero/src/cajero/CajeroMain.java
@@ -0,0 +1,11 @@
+
+package cajero;
+
+public class CajeroMain {
+
+    public static void main(String[] args) {
+        Cajero cajero = new Cajero();
+        cajero.setVisible(true);
+    }
+    
+}
